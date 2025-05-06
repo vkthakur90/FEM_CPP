263b5996450adec78ee788898f9fdb21462d43f9
Vinay Kumar Thakur <33784008+vkthakur90@users.noreply.github.com>
2025-05-06 21:52:43 +0530

    Add files via upload

--- outputs/input_script.py
+++ outputs/input_script.py
@@ -3,19 +3,19 @@ import femsolver as fs
 # create solver
 sol = fs.FemSolver_f100() 
 
-sol.length_x(1.0)
-sol.length_y(2.0)
+sol.length_x = 1.0
+sol.length_y = 2.0
 
 # set material tensors and boundary conditions
-sol.k_xx(0.1)
-sol.k_xy(0.0)
-sol.k_yy(0.1)
-sol.q_rate(1.0)
+sol.k_xx = 0.1
+sol.k_xy = 0.0
+sol.k_yy = 0.1
+sol.q_rate = 1.0
 
-sol.boundary_top(0.0)
-sol.boundary_bottom(0.0)
-sol.boundary_left(0.0)
-sol.boundary_right(0.0)
+sol.boundary_top = 0.0
+sol.boundary_bottom = 0.0
+sol.boundary_left = 0.0
+sol.boundary_right = 0.0
 
 # run the FEM solve
 sol.compute()
